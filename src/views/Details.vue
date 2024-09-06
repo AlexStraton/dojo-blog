0cdf2190ccaa0751b3f389ca2092b771025f5c32
Alex Straton <alexandrastraton1@gmail.com>
2024-09-06 12:29:47 +0100

    added router-link to the single post page

---
+++ src/views/Details.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Details Page</h1>
+    <p>Showing details for post ID: {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  setup(props) {
+    console.log(props.id);
+  },
+};
+</script>
+
+<style></style>

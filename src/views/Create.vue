2dc59187575b97cc9ce6745d861074670e0e1204
Alex Straton <alexandrastraton1@gmail.com>
2024-09-06 16:23:41 +0100

    styled create view

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,19 +1,13 @@
 <template>
-  <h2>Create</h2>
+  <h2 class="header">Create</h2>
   <form>
-    <label>
-      Title:
-      <input v-model="title" required />
-    </label>
-    <label>
-      Content:
-      <input v-model="content" required />
-    </label>
-    <label>
-      Tags (hit enter to add a tag):
-      <input @keydown.enter.prevent="addTag" v-model="tag" required />
-    </label>
-    <div v-for="tag in tags" :key="tag">{{ tag }}</div>
+    <label> Title: </label>
+    <input v-model="title" required />
+    <label> Content: </label>
+    <textarea v-model="content" required />
+    <label> Tags (hit enter to add a tag): </label>
+    <input @keydown.enter.prevent="addTag" v-model="tag" required />
+    <div v-for="tag in tags" :key="tag" class="tag">#{{ tag }}</div>
     <button>Add Post</button>
   </form>
 </template>
@@ -28,8 +22,7 @@ export default {
     const tags = ref([]);
 
     function addTag() {
-      console.log(tag.value, tags.value);
-      if (tags.value.includes(tag.value)) {
+      if (!tags.value.includes(tag.value)) {
         tags.value.push(tag.value);
       }
       tag.value = "";
@@ -40,4 +33,68 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.header {
+  color: #1f52fa;
+  font-size: xx-large;
+  text-shadow: 6px 6px #b6a9a9;
+}
+form {
+  max-width: 480px;
+  margin: 0 auto;
+  text-align: left;
+}
+input,
+textarea {
+  display: block;
+  border-radius: 10px;
+  margin: 10px 0;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
+  border: 1px solid #a97676;
+}
+textarea {
+  height: 160px;
+}
+label {
+  display: inline-block;
+  margin-top: 30px;
+  position: relative;
+  font-size: 20px;
+  color: white;
+  margin-bottom: 10px;
+}
+label::before {
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #1f52fa;
+  position: absolute;
+  z-index: -1;
+  padding-right: 40px;
+  left: -30px;
+  transform: rotateZ(-1.5deg);
+}
+
+button {
+  display: block;
+  border-radius: 20px;
+  margin-top: 20px;
+  background: #1f52fa;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  font-size: 18px;
+}
+.tag {
+  display: inline-block;
+  margin: 10px 10px 0 0;
+  color: #444;
+  background: #ddd;
+  padding: 8px;
+  border-radius: 20px;
+  font-size: 14px;
+}
+</style>

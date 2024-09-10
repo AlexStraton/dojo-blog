f5143c9baf9297e27ce114bc1fd9ff6bc87b9e12
Alex Straton <alexandrastraton1@gmail.com>
2024-09-10 07:46:12 +0100

    created a tagCloud component and passed all the posts objects as a prop from home

---
+++ src/components/TagCloud.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tag-cloud">ALL TAGS</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    props: ["posts"];
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
   <div v-if="error">{{ error }}</div>
   <div v-if="posts.length">
     <PostList :posts="posts" />
+    <TagCloud :posts="posts" />
   </div>
   <div v-else>
     <Spinner />
@@ -12,12 +13,12 @@
 <script>
 import PostList from "@/components/PostList.vue";
 import getPosts from "@/composables/getPosts";
-
+import TagCloud from "@/components/TagCloud.vue";
 import Spinner from "@/components/Spinner.vue";
 
 export default {
   name: "Home",
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
     const { posts, error, load } = getPosts();
     load();

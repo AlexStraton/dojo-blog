f504a9af9f254ad9f4744fad3667bfa92a8d9c8b
Alex Straton <alexandrastraton1@gmail.com>
2024-09-10 08:22:29 +0100

    displayed each tag in a router-link so that when it is clicked, it takes the user to all the blog articles with that tag

--- src/components/TagCloud.vue
+++ src/components/TagCloud.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="tag-cloud">ALL TAGS</div>
+  <div v-for="tag in tags" :key="tag">
+    <router-link :to="{ name: 'tag', params: { tag: tag } }"
+      >#{{ tag }}</router-link
+    >
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-  setup() {
-    props: ["posts"];
+  props: ["posts"],
+  setup(props) {
+    const tags = ref([]);
+    const tagSet = new Set();
+    const allTags = props.posts.forEach((blog) =>
+      blog.tags.forEach((tag) => tagSet.add(tag))
+    );
+    tags.value = [...tagSet];
+    return { tags };
   },
 };
 </script>
